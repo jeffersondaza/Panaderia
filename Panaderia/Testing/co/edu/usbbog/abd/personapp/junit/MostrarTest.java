da1b09e917d5217cde299f1243a44ff402b159f4
jeffersondazacaro <60698493+jeffersondazacaro@users.noreply.github.com>
2020-05-20 09:51:28 -0500

    Add files via upload

--- Panaderia/Testing/co/edu/usbbog/abd/personapp/junit/MostrarTest.java
+++ Panaderia/Testing/co/edu/usbbog/abd/personapp/junit/MostrarTest.java
@@ -20,17 +20,20 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import co.edu.usbbog.abd.personapp.controller.persistence.ConexionMySQL;
 import co.edu.usbbog.abd.personapp.controller.persistence.Mostrar;
-
+import co.edu.usbbog.abd.personapp.controller.persistence.ProductoDAO;
+import co.edu.usbbog.abd.personapp.model.Producto;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @TestInstance(Lifecycle.PER_CLASS)
 class MostrarTest {
 
 	Mostrar m = new Mostrar();
+	ProductoDAO p = new ProductoDAO();
 
 	@Tag("Primeras pruebas")
+
 	@Timeout(1000)
-	@RepeatedTest(3)
+	@RepeatedTest(1)
 	@DisplayName("Test de repeticiones")
 	void test() {
 		// fail("Not yet implemented");
@@ -38,7 +41,7 @@ class MostrarTest {
 		m.visualizarGanancias();
 
 	}
-	
+
 	@Disabled
 	@Test
 	void test_2() {
@@ -50,12 +53,11 @@ class MostrarTest {
 		assertTrue(expected);
 
 	}
-	
-	
-	//@BeforeEach
-	//@AfterEach
-	//@BeforeAll
-	//@AfterAll
+
+	// @BeforeEach
+	// @AfterEach
+	// @BeforeAll
+	// @AfterAll
 	@Tag("Segundas Pruebas")
 	@Timeout(10)
 	@Test
@@ -68,5 +70,88 @@ class MostrarTest {
 		assertTrue(expected);
 
 	}
+
+	@Test
+	void test_4() {
+		Producto pro = new Producto("Helado", "Jet", "07/08/2020", 54, 2);
+
+		assertEquals(p.registrarProducto(pro), true);
+
+	}
+
+	@Test
+	void test_5() {
+		Producto pro = new Producto("Helado", "Jet", "07/08/2020", 54, 2);
+
+		assertFalse(p.registrarProducto(pro));
+
+	}
+
+	@Test
+	void test_6() {
+		Producto pro = new Producto("Helado", "Polet", "07/08/2020", 54, 2);
+
+		boolean prueba = p.actualizarProducto(pro);
+		assertNotNull(pro);
+
+	}
+	
+	@Test
+	void test_7() {
+		String nombre="Helado";
+
+		boolean prueba = p.eliminar(nombre);
+		assertNull(prueba);
+
+	}
+
+	
+
+
+	/**
+	 * 
+	 * @BeforeEach @Order(1) void agregadoDeElementos() { assert
+	 *             productoN1.getCosto() > 0 : "El Precio debe ser mayor a 0";
+	 *             assert productoN2.getCosto() > 0 : "El Precio debe ser mayor a
+	 *             0";
+	 * 
+	 *             assertTrue("Elemento a�adido",
+	 *             controladorProductos.agregarProducto(productoN1));
+	 *             assertTrue("Elemento a�adido",
+	 *             controladorProductos.agregarProducto(productoN2)); }
+	 * 
+	 * @Test @Order(2) void agregadoVerificacion() {
+	 *       controladorProductos.agregarProducto(productoN3); }
+	 * 
+	 * @AfterEach @Order(3) void verificacionAgregaso() { boolean expected = false;
+	 *            boolean current =
+	 *            controladorProductos.agregarProducto(productoN3);
+	 *            assertEquals(expected, current); }
+	 * 
+	 * @BeforeEach @Order(4) public void eliminarProductosAgregados() {
+	 *             controladorProductos.eliminarProducto(productoN1);
+	 *             controladorProductos.eliminarProducto(productoN2); }
+	 * 
+	 * @AfterEach @Order(5) void mostrarProductos() {
+	 *            controladorProductos.darProductos(); }
+	 * 
+	 * @After @Order(6) void verCantidad() {
+	 *        assertEquals(controladorProductos.getTotalProductos(), 8); }
+	 * 
+	 * @After @Order(7) void productoEncontrado() { assertSame(productoN1,
+	 *        controladorProductos.buscarPorNombre(productoN3.getNombre())); }
+	 * 
+	 * @AfterEach @Order(8) void buscar() {
+	 *            assertNotNull(controladorProductos.buscarPorNombre(productoN3.getNombre()));
+	 *            }
+	 * 
+	 * @AfterEach @Order(9) void verificarExistencia() {
+	 *            assertTrue(controladorProductos.valirdarExistente(productoN3),
+	 *            "Fue agregado"); }
+	 * 
+	 * @AfterEach @Order(9) void verificarTiempoAgregado() {
+	 *            assertTimeout(ofMinutes(2), () -> { // Perform task that takes
+	 *            less than 2 minutes. }); }
+	 */
 
 }
